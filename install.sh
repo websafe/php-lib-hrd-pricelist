3fa085b0745d4a955cd5691768c7b88c8c2fa93f
Thomas Szteliga <ts@websafe.pl>
2013-09-12 08:32:43 +0200

    Modified installer.

--- install.sh
+++ install.sh
@@ -4,9 +4,19 @@
 mkdir -p vendor/bin
 
 #
-php -n \
+php \
+    -n \
+    -d allow_url_fopen=yes \
+    -d suhosin.executor.include.whitelist=phar \
     -r "eval('?>'.file_get_contents('https://getcomposer.org/installer'));" \
     -- --install-dir=vendor/bin
 
 #
-php -n vendor/bin/composer.phar update
+php \
+    -n \
+    -d allow_url_fopen=yes \
+    -d suhosin.executor.include.whitelist=phar \
+    vendor/bin/composer.phar update \
+    --no-dev \
+    --prefer-dist \
+    --optimize-autoloader
